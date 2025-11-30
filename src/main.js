b5f3209fb9254eb94f247e08cc75f1d505bbc76f
Nethan Nagendran <nethan.nagendran@gmail.com>
2025-11-29 19:28:46 -0500

    updated major and minor keys

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    resizable: false, 
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
